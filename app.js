74747092d464e1e3fb9f538f67d75591202ee350
James Quoc Thai NGUYEN CAO <nguyencaojqt@gmail.com>
2025-05-07 12:41:42 +0200

    test

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
 app.use('/users', usersRouter);
 app.use('/tweets', tweetsRouter);
 
